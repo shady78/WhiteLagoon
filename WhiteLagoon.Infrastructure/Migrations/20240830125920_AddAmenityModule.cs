3ee8fe31cc9aa9e82bde4bf7b01d297786829281
shady khalifa <shadykhalifa.dotnetdev@gmail.com>
2024-08-30 16:26:54 +0300

    ⭐️  Amenity Module ⭐️

---
+++ WhiteLagoon.Infrastructure/Migrations/20240830125920_AddAmenityModule.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmenityModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VillaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Amenities_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "Id", "Description", "Name", "VillaId" },
+                values: new object[,]
+                {
+                    { 1, null, "Private Pool", 1 },
+                    { 2, null, "Microwave", 1 },
+                    { 3, null, "Private Balcony", 1 },
+                    { 4, null, "1 king bed and 1 sofa bed", 1 },
+                    { 5, null, "Private Plunge Pool", 2 },
+                    { 6, null, "Microwave and Mini Refrigerator", 2 },
+                    { 7, null, "Private Balcony", 2 },
+                    { 8, null, "king bed or 2 double beds", 2 },
+                    { 9, null, "Private Pool", 3 },
+                    { 10, null, "Jacuzzi", 3 },
+                    { 11, null, "Private Balcony", 3 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 30, 15, 59, 19, 314, DateTimeKind.Local).AddTicks(576));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 30, 15, 59, 19, 314, DateTimeKind.Local).AddTicks(623));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 30, 15, 59, 19, 314, DateTimeKind.Local).AddTicks(626));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Amenities_VillaId",
+                table: "Amenities",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 27, 17, 35, 14, 670, DateTimeKind.Local).AddTicks(6535));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 27, 17, 35, 14, 670, DateTimeKind.Local).AddTicks(6583));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 8, 27, 17, 35, 14, 670, DateTimeKind.Local).AddTicks(6586));
+        }
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/AmenityController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Web.ViewModels;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AmenityController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AmenityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            // ToDO Include by use UnitOfWork
+
+            var amenities = _unitOfWork.Amenity.GetAll(includeProperties: "Villa");
+            return View(amenities);
+        }
+
+        public IActionResult Create()
+        {
+            AmenityVM amenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AmenityVM obj)
+        {
+            //ModelState.Remove("Villa");
+            bool amenityExists = _unitOfWork.Amenity.Any(u => u.Id == obj.Amenity.Id);
+            //bool isNumberUnique = _context.VillaNumbers.Where(u => u.Villa_Number == obj.Villa_Number).Count() == 0;
+
+            if (ModelState.IsValid && !amenityExists)
+            {
+                _unitOfWork.Amenity.Add(obj.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (amenityExists)
+            {
+                TempData["Error"] = "The amenity already exists.";
+            }
+            obj.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(obj);
+
+        }
+
+        public IActionResult Update(int amenityId)
+        {
+           AmenityVM amenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(x => x.Id == amenityId)
+            };
+            if (amenityVM.Amenity is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Update(AmenityVM amenityVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Update(amenityVM.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            amenityVM.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(amenityVM);
+
+        }
+
+        public IActionResult Delete(int amenityId)
+        {
+            AmenityVM amenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(x => x.Id == amenityId)
+            };
+            if (amenityVM.Amenity is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(amenityVM);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Delete(AmenityVM amenityVM)
+        {
+            Amenity amenity = _unitOfWork.Amenity
+                .Get(x => x.Id == amenityVM.Amenity.Id);
+
+            if (amenity is not null)
+            {
+                _unitOfWork.Amenity.Remove(amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been deleted successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["error"] = "The amenity could not be deleted.";
+
+            return View();
+        }
+
+
+
+    }
+}
