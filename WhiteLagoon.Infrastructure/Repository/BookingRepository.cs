75e7f13c410bcf115c2f447042870484e635589e
shady khalifa <shadykhalifa.dotnetdev@gmail.com>
2024-09-04 12:40:57 +0300

    Start Booking

---
+++ WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookingRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Booking entity)
+        {
+            _context.Bookings.Update(entity);
+        }
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
