f4e1c1b7b9d082d280b69da4cf4d30a0fa32328d
shady khalifa <shadykhalifa.dotnetdev@gmail.com>
2024-10-05 19:39:28 +0300

    Dashboard and Charts

---
+++ WhiteLagoon.Web/ViewModels/RedialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WhiteLagoon.Web.ViewModels
+{
+    public class RedialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal CountInCurrentMonth { get; set; }
+        public bool HasRatioIncreased { get; set; }
+        public int[] Series { get; set; }
+    }
+}
